eae0e882ba46cb847f75a4225830afc18300fb25
Dushyant <dushyantyaduvanshi06@gmail.com>
2026-09-07 17:54:35 +0530

    fix: update server shutdown logic to properly close the server before disconnecting from the database

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ require('dotenv').config()
 const app = require('./app');
 const prisma = require('./src/config/db');
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log("Server is running...");
 });
 
-process.on('SIGINT' , async()=>{
-    await prisma.$disconnect();
-    process.exit(0);
+process.on('SIGINT', async () => {
+    server.close(async () => {
+        await prisma.$disconnect();
+        process.exit(0);
+    })
 });
 
-process.on("SIGTERM", async()=>{
-    await prisma.$disconnect();
-    process.exit(0);
+process.on("SIGTERM", async () => {
+    server.close(async () => {
+        await prisma.$disconnect();
+        process.exit(0);
+    })
 });
 
